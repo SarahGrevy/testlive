6d85afd81d7909dd293d1997d24933fb99891270
SarahGrevy <65611758+SarahGrevy@users.noreply.github.com>
2020-06-09 15:42:48 -0400

    Update pages.py

--- appy/pages.py
+++ appy/pages.py
@@ -2,17 +2,6 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
-class MyPage(Page):
-    live_method = 'live_auction'
-    
-    form_model = 'player'
-    #form_fields = ['name', 'age']
-
-class Page_2(Page):
-    form_model = 'player'
-
-    form_fields = ['individual_judgment']
-
 class Page_3(Page):
     form_model = 'player'
 
@@ -26,7 +15,5 @@ class Results(Page):
 
 
 page_sequence = [
-    MyPage,
-    Page_2,
     Page_3
 ]
