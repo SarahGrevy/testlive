60c5ff66cea08f84dd2d8c9c088eec2397c8cdbb
SarahGrevy <65611758+SarahGrevy@users.noreply.github.com>
2020-06-09 15:55:30 -0400

    Update pages.py

--- appy/pages.py
+++ appy/pages.py
@@ -2,7 +2,7 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
-class Judgement1(Page):
+class Judgement_1(Page):
     form_model = 'player'
     form_fields = ['judgement1']
 
