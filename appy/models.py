7dc089d09acc134e08d6db856cb2bb6416a4e5c2
SarahGrevy <65611758+SarahGrevy@users.noreply.github.com>
2020-06-09 16:00:53 -0400

    Update models.py

--- appy/models.py
+++ appy/models.py
@@ -51,5 +51,11 @@ class Player(BasePlayer):
     age = models.IntegerField()
     individual_judgment = models.IntegerField()
     mutual_judgment = models.IntegerField()
+    PartnerAgreement = models.StringField(choices=[['Strongly disagree', 'Strongly disagree'], ['Somewhat disagree ', 'Somewhat disagree '], ['Disagree ', 'Disagree '], ['Neither agree nor disagree', 'Neither agree nor disagree'], ['Somewhat agree', 'Somewhat agree'], ['Agree', 'Agree']], label='I think that my partner and I are on the same wavelength with regard to our judgments of the video', widget=widgets.RadioSelectHorizontal)
     judgement1 = models.FloatField()
+    certainty1 = models.FloatField()
+    judgement2 = models.FloatField()
+    certainty2 = models.FloatField()
+    judgement3 = models.FloatField()
+    certainty3 = models.FloatField()
 
